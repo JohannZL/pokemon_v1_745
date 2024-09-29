001eb659b76bf38b2ccd5e49f1379d3f030c1ea6
JohannZL <jodzambranol@udistrital.edu.co>
2024-09-29 14:56:25 -0500

    La liga pokemon

--- src/main/java/Fight.java
+++ src/main/java/Fight.java
@@ -8,28 +8,30 @@ public class Fight {
         Pokemon p1 = new Pokemon("charmander", Type.fire, new Power[]{po1, po2});
         Pokemon p2 = new Pokemon("Pikachu", Type.electric, new Power[]{po3, po4});
 
-
-        fight2pokemons(p1, p2);
-        fight2pokemons(p2, p1);
-        fight2pokemons(p1, p2);
-        fight2pokemons(p2, p1);
-        fight2pokemons(p1, p2);
-        fight2pokemons(p2, p1);
-        fight2pokemons(p1, p2);
-        fight2pokemons(p2, p1);
-        fight2pokemons(p1, p2);
-        fight2pokemons(p2, p1);
-
+            fight2pokemons(p1, p2);
 
     }
 
-    public static void fight2pokemons(Pokemon pokemon1, Pokemon pokemon2) {
-        double damage = pokemon2.attack();
-        pokemon1.setLife(pokemon1.getLife() - damage);
-        //System.out.println(pokemon1.getLife());
-        System.out.println("Defiende: " + pokemon1);
-        System.out.println("Ataca: " + pokemon2);
-
+    public static void fight2pokemons(Pokemon pokemon1, Pokemon pokemon2)
+    {
+        do {
+            Pokemon aux;
+            double damage = pokemon2.attack();
+            pokemon1.setLife(pokemon1.getLife() - damage);
+            //System.out.println(pokemon1.getLife());
+            System.out.println("Defiende: " + pokemon1);
+            System.out.println("Ataca: " + pokemon2);
+            aux=pokemon1;
+            pokemon1=pokemon2;
+            pokemon2=aux;
+        }while(pokemon1.getLife() > 0 && pokemon2.getLife() > 0);
+        if(pokemon1.getLife() <0)
+        {
+            System.out.println("Gano"+" " +pokemon2);
+        }else
+        {
+            System.out.println("Gano"+" " +pokemon1);
+        }
     }
 
 }
